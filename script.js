7d3b3632f8638ec122aa76043be52b4bc8a4aa65
Natanael Rodrigues <131223610+Natanael016@users.noreply.github.com>
2026-03-12 01:10:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Site carregado com sucesso!");
+const busca = document.getElementById("buscar");
+
+const produtos = document.querySelectorAll(".produto");
+
+function removerAcento(texto) {
+  return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+busca.addEventListener("keyup", function () {
+  const texto = removerAcento(busca.value.toLowerCase());
+
+  produtos.forEach(function (produto) {
+    const nome = removerAcento(produto.innerText.toLowerCase());
+
+    if (nome.includes(texto)) {
+      produto.style.display = "block";
+    } else {
+      produto.style.display = "none";
+    }
+  });
+});
